d160478bcd71deeb7d1d98e3ea18fd0e83271df2
Filippo Rodrigues <legendragon03453@gmail.com>
2026-03-19 04:31:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3782476318-10160",
+  "appId": "1:857001794068:web:b02336b8c8911d9319a501",
+  "apiKey": "AIzaSyDQJjCnCx2D3pLINQBpPjae4W-W5M6M8u4",
+  "authDomain": "studio-3782476318-10160.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "857001794068"
+};
